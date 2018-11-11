fc2d5c6f500c46cdffd1136b6a36967d13f9d8d8
Naz Malik <nazmalik08@gmail.com>
2018-11-11 16:56:28 +0000

    output due stats

--- puppeteer.js
+++ puppeteer.js
@@ -1,45 +1,81 @@
+"use strict";
+
 require('dotenv').config();
 const puppeteer = require('puppeteer');
+const moment = require('moment');
+
+const DAYS_SOON = 5;
+const LIBRARY_WEBSITE = "https://capitadiscovery.co.uk/islington/account";
+
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://capitadiscovery.co.uk/islington/account');
+  console.log("👋 hello there\n")
 
-  // enter deets
+  // Visit library website
+  console.log("🚀 launching browser")
+  const browser = await puppeteer.launch();
+  console.log("🚶 heading over to the library\n")
+  const page = await browser.newPage();
+  await page.goto(LIBRARY_WEBSITE);
+
+
+  // Log in 
+  console.log("🔐 logging in")
   await page.type('#borrowerBarcodeTextBox', process.env.BORROWER_BARCODE);
   await page.type('#pinTextBox', process.env.PIN);
 
-  // login
   await page.evaluate( () =>
   {
     Array.from( document.querySelectorAll('input') ).filter( element => element.name === 'borrowerLoginButton' )[0].click();
   });
-
   await page.waitForNavigation();
+  console.log("✅ logged in!\n")
 
-  // get renewal dates
-  // todo: get loan rows
-  const tds = await page.evaluate(() => {
-    const anchors = document.getElementsByClassName('accDue');
-    return [].map.call(anchors, a => a.innerText);
+
+  // Get due dates from html
+  const dueDates = await page.evaluate(() => {
+    return Array.from( document.getElementsByClassName('accDue')).map(a => a.innerText) 
   });
+  dueDates.shift() // drop the header
 
-  console.log(tds);
+  // Output loans due soon
+  let dueSoon = []
 
-  // filter those due in next 5 days
+  dueDates.forEach(d => {
+    d = d.replace('\t', '')
+
+    const loanDue = moment(d, "DD MMMM")
+    const now = moment()
+
+    const diff = moment.duration(loanDue - now)
+    const daysTillDue = diff.days()
+    // console.log(`${diff.days()} days, ${diff.hours()} hours`)
+
+    if (daysTillDue < DAYS_SOON) {
+      dueSoon.push(daysTillDue)
+    }
+  })
+
+  dueSoon.sort(sortNumbers)
+
+  console.log(`Loans oustanding: ${dueDates.length}`);
+  console.log(`Due in next ${DAYS_SOON} days: ${dueSoon.length}`)
+  console.log(`Soonest due: ${dueSoon[0] || 'n/a'} days\n`)
+
 
   // if one has reached max renewals, log it
   // loans.each((l) => l.)
 
+  // prompt to renew y/n
   // renew all that can be renewed
   // await page.click()
-
-  // check none due in next 5 days
-
 
 
   await page.screenshot({path: 'screenshots/loans.png'});
 
   await browser.close();
 })();
+
+function sortNumbers(a, b) {
+  return a - b
+}
